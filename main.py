bdd9af63f75cba174e337e03ca39be5f67c50118
TheSoulsHunter <konrad.romanski4@gmail.com>
2021-01-23 01:48:57 +0100

    passing values and buttons functions added

--- main.py
+++ main.py
@@ -31,26 +31,44 @@ sd = {
     'e' : 0.6,
     'W' : 4,
     'S' : 2
-
 }
 
+# sets values
 @app.route('/getmethod/<jsdata>')
 def get_javascript_data(jsdata):
-    print(f"### {jsdata}")
+    # print(f"###[PYLOG] {jsdata}")
+    jsdata_list = list(map(float, jsdata.split(',')))
+    # print(f"{jsdata_list}")
+    sd['n'] = jsdata_list[0];
+    sd['T_star'] = jsdata_list[1];
+    sd['T_zero'] = jsdata_list[2];
+    sd['T_amb'] = jsdata_list[3];
+    sd['kp'] = jsdata_list[4];
+    sd['Tp'] = jsdata_list[5];
+    sd['Ti'] = jsdata_list[6];
+    sd['Td'] = jsdata_list[7];
+    sd['A'] = jsdata_list[8];
+    sd['e'] = jsdata_list[9];
+    sd['W'] = jsdata_list[10];
+    sd['S'] = jsdata_list[11];
+    print(f"[LOG] - Received data: {sd}");
+
+    print(f"[LOG] - Updating object...");
+    uar = UAR(sd['n'], sd['T_star'], sd['T_zero'], sd['T_amb'], sd['kp'], sd['Tp'], sd['Ti'], sd['Td'], sd['A'], sd['e'], sd['W'], sd['S'])
+    print(f"[LOG] - done");
     return jsdata
 
+# creates the object
 @app.route('/_generate', methods = ['GET'])
 def generate():
     global uar
     uar = UAR(sd['n'], sd['T_star'], sd['T_zero'], sd['T_amb'], sd['kp'], sd['Tp'], sd['Ti'], sd['Td'], sd['A'], sd['e'], sd['W'], sd['S'])
     return jsonify(msg=1)
 
+# returns next value
 @app.route('/_stuff', methods = ['GET'])
 def stuff():
-    # return jsonify(result=random.randint(0, 10))
-    # for i in range(100):
     a = uar.get_step()
-    # a = uar.get_test()
     print(f"[PYTHON_LOG] - {a}")
     print(f"[LOG] - Next value: {a}")
     return jsonify(result=a)
