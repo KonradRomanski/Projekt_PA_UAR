74328df8e5a1a2b9b9380065e3340c922c18e1e8
LenaPtak <lenkaptak@gmail.com>
2021-02-22 11:51:39 +0100

    'Wskazniki'

--- UAR.py
+++ UAR.py
@@ -3,6 +3,7 @@ import time
 import threading
 import matplotlib.pyplot as plt
 
+
 # A - pole powierzchni - usr def
 # S - wydzelane ciepło w kelwinach/s __ usr def
 # e - emitancja - usr def
@@ -38,17 +39,23 @@ class UAR():
         self.Td = Td
         self.A = A
         self.e = e
-        self.T_historic = [T_zero, T_zero]                  # tymczasowa "baza danych" - wartości początkowe muszą być dostępne
-        self.e_historic = [T_star-T_zero, T_star-T_zero]    # tymczasowa "baza danych" - wartości początkowe muszą być dostępne
+        self.T_historic = [T_zero, T_zero]  # tymczasowa "baza danych" - wartości początkowe muszą być dostępne
+        self.e_historic = [T_star - T_zero,
+                           T_star - T_zero]  # tymczasowa "baza danych" - wartości początkowe muszą być dostępne
         self.S = S
         self.W = W
-        self.sigma = 5.6704*(10**(-8))
-        self.pause = threading.Event()                      # pauza - po uruchomieniu trzeba najpierw ustawić żeby rozpocząć (.set()). Żeby wstrzymać .clear()
-        self.terminate = False                              # ustawienie terminate na true zatrzymuje program
-        self.uMin = 0.5                                     # minimalne wzmocnienie
-        self.uMax = 2                                       # maksymalne wzmocnienie
-        self.n = n           # diagnostyczne
+        self.sigma = 5.6704 * (10 ** (-8))
+        self.pause = threading.Event()  # pauza - po uruchomieniu trzeba najpierw ustawić żeby rozpocząć (.set()). Żeby wstrzymać .clear()
+        self.terminate = False  # ustawienie terminate na true zatrzymuje program
+        self.uMin = 0.5  # minimalne wzmocnienie
+        self.uMax = 2  # maksymalne wzmocnienie
+        self.n = n  # diagnostyczne
         # self.__start__()
+
+        # ----------WSKAŹNIKI JAKOŚCI----
+        self.L_czasRegObl = 0  # do funkcji CzasRegulacji
+        self.L_paramtetrRegulacji = 0.05  # parametr o wartości pomiędzy 0,01 do 0,05, aby obliczyć deltaT, możnaby dodać kontrolkę na stronie, żeby user def
+        self.u_historic = []
 
     def update_values(self, n, T_star, T_zero, T_amb, kp, Tp, Ti, Td, A, e, W, S):
         self.TEST = 0
@@ -61,25 +68,28 @@ class UAR():
         self.Td = Td
         self.A = A
         self.e = e
-        self.T_historic = [T_zero, T_zero]                  # tymczasowa "baza danych" - wartości początkowe muszą być dostępne
-        self.e_historic = [T_star-T_zero, T_star-T_zero]    # tymczasowa "baza danych" - wartości początkowe muszą być dostępne
+        self.T_historic = [T_zero, T_zero]  # tymczasowa "baza danych" - wartości początkowe muszą być dostępne
+        self.e_historic = [T_star - T_zero,
+                           T_star - T_zero]  # tymczasowa "baza danych" - wartości początkowe muszą być dostępne
         self.S = S
         self.W = W
-        self.sigma = 5.6704*(10**(-8))
-        self.pause = threading.Event()                      # pauza - po uruchomieniu trzeba najpierw ustawić żeby rozpocząć (.set()). Żeby wstrzymać .clear()
-        self.terminate = False                              # ustawienie terminate na true zatrzymuje program
-        self.uMin = 0.5                                     # minimalne wzmocnienie
-        self.uMax = 2                                       # maksymalne wzmocnienie
+        self.sigma = 5.6704 * (10 ** (-8))
+        self.pause = threading.Event()  # pauza - po uruchomieniu trzeba najpierw ustawić żeby rozpocząć (.set()). Żeby wstrzymać .clear()
+        self.terminate = False  # ustawienie terminate na true zatrzymuje program
+        self.uMin = 0.5  # minimalne wzmocnienie
+        self.uMax = 2  # maksymalne wzmocnienie
         self.n = n
 
     def __T_n__(self):
-        return self.T_historic[-1] + self.Tp*(self.S * self.__u_n__() - (self.sigma * self.e * self.A * ((self.T_historic[-1]**4) - (self.T_amb**4)) * self.W))
+        return self.T_historic[-1] + self.Tp * (self.S * self.__u_n__() - (
+                    self.sigma * self.e * self.A * ((self.T_historic[-1] ** 4) - (self.T_amb ** 4)) * self.W))
 
     def __e_n__(self):
         return self.T_star - self.T_historic[-1]
 
     def __u_n__(self):
-        temp = self.kp*(self.e_historic[-2] + (self.Tp/self.Ti) * sum(self.e_historic) + self.Td/self.Tp*(self.e_historic[-1] - self.e_historic[-2]))
+        temp = self.kp * (self.e_historic[-2] + (self.Tp / self.Ti) * sum(self.e_historic) + self.Td / self.Tp * (
+                    self.e_historic[-1] - self.e_historic[-2]))
         if (temp < self.uMin):
             return self.uMin
         elif (temp > self.uMax):
@@ -88,13 +98,13 @@ class UAR():
             return temp
 
     def __build_data__(self):
-        val = 0 #diagnostyczne
+        val = 0  # diagnostyczne
 
-        while val < self.n: # do celów diagnostycznych
-        # while True:        # odkomentować gdy będą przyciski do wznowienia i stopu (sprawdzić i wykorzystać ich implementację)
+        while val < self.n:  # do celów diagnostycznych
+            # while True:        # odkomentować gdy będą przyciski do wznowienia i stopu (sprawdzić i wykorzystać ich implementację)
             self.e_historic.append(self.__e_n__())
 
-            #self.S = 0.05 * self.__u_n__()
+            # self.S = 0.05 * self.__u_n__()
             temp = self.__T_n__()
             # print(temp)
             self.T_historic.append(temp)
@@ -103,7 +113,7 @@ class UAR():
                 self.T_historic.clear()
                 self.e_historic.clear()
                 break
-            val += 1 # diagnostyczne
+            val += 1  # diagnostyczne
 
     def __start__(self):
         t = threading.Thread(name="process", target=self.__build_data__)
@@ -121,11 +131,70 @@ class UAR():
 
     def get_step(self):
         self.e_historic.append(self.__e_n__())
+        self.u_historic.append(self.__u_n__())
         temp = self.__T_n__()
         self.T_historic.append(temp)
         return temp
 
-#TO JEST INNA WERSJA
     def get_test(self):
         self.TEST += 1
         return self.TEST
+
+    # --------WSKAŹNIKI JAKOŚCI---------------------------------------------------------------------------------------
+
+    # Funkcja zwraca uchyb ustalony w momencie, kiedy już się ustali. Przed tym nie zwraca nic.
+    # Uchyb ustalony mamy wtedy, kiedy temp przyjmuje wartości <= o dany % T_star
+    def uchybUstalony(self, licznik):
+        ileProcent = 5  # granica
+        granica = abs(ileProcent * (self.T_zero - self.T_star) / 100)
+        if licznik > 1:
+            # sprawdzamy, czy jest przełamanie fali
+            if abs((self.T_star - self.T_historic[licznik - 1])) > abs((self.T_star - self.T_historic[licznik])) > abs(
+                    (self.T_star - self.T_historic[licznik - 2])):
+                if abs((self.T_star - self.T_historic[licznik - 1])) < granica:
+                    return abs(self.T_star - self.T_historic[licznik - 1])
+
+    # stała wartość, teoretycznie powinna być w %, więc można return pomnożyć razy 100
+    def przeregulowanie(self, licznik):
+        if licznik > 1:
+            # sprawdzamy, czy jest przełamanie fali
+            if abs((self.T_star - self.T_historic[licznik - 1])) > abs((self.T_star - self.T_historic[licznik])) > abs(
+                    (self.T_star - self.T_historic[licznik - 2])):
+                return abs((max(self.T_historic) - self.T_star) / self.T_star)
+
+    # stała wartość, zwraca n*okres próbkowania
+    def czasRegulacji(self, licznik):
+        if self.L_czasRegObl == 0:
+            deltaT = self.L_paramtetrRegulacji * self.T_star
+            if licznik > 1:
+                # sprawdzamy, czy jest przełamanie fali
+                if abs((self.T_star - self.T_historic[licznik - 1])) > abs(
+                        (self.T_star - self.T_historic[licznik])) > abs((self.T_star - self.T_historic[licznik - 2])):
+                    # Czy musi być and tutaj?
+                    if (self.T_star - deltaT) <= self.T_historic[licznik] <= (self.T_star + deltaT):
+                        self.L_czasRegObl = licznik * self.Tp
+                        return self.L_czasRegObl
+
+    def dokladnoscRegulacji_e(self):
+        suma = 0
+        for n in self.e_historic:
+            suma += abs(n)
+        return self.Tp * suma
+
+    def dokladnoscRegulacji_e2(self):
+        suma = 0
+        for n in self.e_historic:
+            suma += n ** 2
+        return self.Tp * suma
+
+    def kosztyRegulacji_u(self):
+        suma = 0
+        for n in self.u_historic:
+            suma += abs(n)
+        return self.Tp * suma
+
+    def kosztyRegulacji_u2(self):
+        suma = 0
+        for n in self.u_historic:
+            suma += n ** 2
+        return self.Tp * suma
